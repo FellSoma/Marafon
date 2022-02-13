8e8d8a7064ca1eb994be397f5bc6a65c7a74b05a
FellSoma <n.felix2003@gmail.com>
2022-02-13 17:43:19 +0500

    add layouts number 23-24.

---
+++ Marafonchik/Marafon/RunnerControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Marafon
+{
+    /// <summary>
+    /// Логика взаимодействия для RunnerControl.xaml
+    /// </summary>
+    public partial class RunnerControl : Window
+    {
+        public RunnerControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void editRunnerProfile(object sender, RoutedEventArgs e)
+        {
+            Window w = new EditRunnerProfile();
+            w.Show();
+        }
+    }
+}
